128f53cc1a04f1a7b5f4e4e852c49b9ee785a70f
jordanburdett <jordan@burdett.us>
2019-05-14 12:52:33 -0600

    Said Hi back...

--- src/dave/Main.java
+++ src/dave/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is David Moreno.");
+        System.out.println("Hello Dave, my name is Jordan");
     }
 }
